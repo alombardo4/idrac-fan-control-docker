82d94b6733a14f04dd22b90e8ca03a82d651a994
tigerblue77 <37409593+tigerblue77@users.noreply.github.com>
2022-04-11 19:01:44 +0200

    Local iDRAC management

--- startup.sh
+++ startup.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
 echo $IDRAC_HOST >> /host.txt
 echo $IDRAC_USER >> /user.txt
 echo $IDRAC_PW >> /pw.txt
 echo $FANSPEED >> /fanspeed.txt
 
 echo "Host: `cat /host.txt`"
-echo "User: `cat /user.txt`"
-echo "PW: `cat /pw.txt`"
+if [[ $IDRAC_HOST != "local" ]]
+then
+  echo "User: `cat /user.txt`"
+  echo "PW: `cat /pw.txt`"
+fi
 echo "Fan speed `cat /fanspeed.txt`"

c30bbad7fe6cc14e3772162d23fac6e0510220bf
Tigerblue77 <37409593+tigerblue77@users.noreply.github.com>
2023-02-04 14:14:20 +0000

    Added IPMI device presence check in local mode

--- Dell_iDRAC_fan_controller.sh
+++ Dell_iDRAC_fan_controller.sh
@@ -74,6 +74,11 @@ echo "iDRAC/IPMI host: $IDRAC_HOST"
 # Check if the iDRAC host is set to 'local' or not then set the IDRAC_LOGIN_STRING accordingly
 if [[ $IDRAC_HOST == "local" ]]
 then
+  # Check that the Docker host IPMI device (the iDRAC) has been exposed to the Docker container
+  if [ ! -e "/dev/ipmi0" ] && [ ! -e "/dev/ipmi/0" ] && [ ! -e "/dev/ipmidev/0" ]; then
+    echo "/!\ Could not open device at /dev/ipmi0 or /dev/ipmi/0 or /dev/ipmidev/0, check that you added the device to your Docker container or stop using local mode. Exiting." >&2
+    exit 1
+  fi
   IDRAC_LOGIN_STRING='open'
 else
   echo "iDRAC/IPMI username: $IDRAC_USERNAME"
